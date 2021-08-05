811fadb18390df896a785471b98e3f9bc6ef4995
Parikshit5504 <desaip932@gmail.com>
2021-08-05 23:32:12 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
+import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
+import { FiltersComponent } from './shoppingcart/filters/filters.component';
+import { ProductListComponent } from './shoppingcart/product-list/product-list.component';
+import { CartComponent } from './shoppingcart/cart/cart.component';
+import { CartItemComponent } from './shoppingcart/cart/cart-item/cart-item.component';
+import { ProductItemComponent } from './shoppingcart/product-list/product-item/product-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    ShoppingcartComponent,
+    FiltersComponent,
+    ProductListComponent,
+    CartComponent,
+    CartItemComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
